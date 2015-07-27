ceae1933d0d025bf746ea8d2f364c90600425fb8
kitecat <kitecat.kitekat@gmail.com>
2015-07-27 10:32:03 +0900

    first version

---
+++ app/src/main/java/com/test/todolist/MainActivity.java
@@ -0,0 +1,139 @@
+package com.test.todolist;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class MainActivity extends FragmentActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final int CM_DELETE_ID = 1;
+
+    private EditText editText;
+
+    final String ATTRIBUTE_NAME_TEXT = "text";
+
+    ListView listView;
+    DB db;
+    SimpleCursorAdapter scAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+
+        db = new DB(this);
+        db.open();
+
+        String[] from = new String[] { DB.COLUMN_IMG, DB.COLUMN_TXT };
+        int[] to = new int[] { R.id.ivImg, R.id.tvText };
+
+        scAdapter = new SimpleCursorAdapter(this, R.layout.item, null, from, to, 0);
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(scAdapter);
+
+        registerForContextMenu(listView);
+
+        getSupportLoaderManager().initLoader(0, null, this);
+    }
+
+    public void onButtonClick(View view) {
+        String note = editText.getText().toString();
+        db.addRec(note, R.drawable.ic_launcher);
+        getSupportLoaderManager().getLoader(0).forceLoad();
+    }
+
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0, CM_DELETE_ID, 0, R.string.delete_record);
+    }
+
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getItemId() == CM_DELETE_ID) {
+            AdapterView.AdapterContextMenuInfo acmi = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+            db.delRec(acmi.id);
+            getSupportLoaderManager().getLoader(0).forceLoad();
+            return true;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        db.close();
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle bndl) {
+        return new MyCursorLoader(this, db);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        scAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+    }
+
+    static class MyCursorLoader extends CursorLoader {
+
+        DB db;
+
+        public MyCursorLoader(Context context, DB db) {
+            super(context);
+            this.db = db;
+        }
+
+        @Override
+        public Cursor loadInBackground() {
+            Cursor cursor = db.getAllData();
+            try  {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return cursor;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
